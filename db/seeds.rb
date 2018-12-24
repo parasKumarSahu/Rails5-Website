5cad1038c5ae20543a08998be4ad32c6d3050bd3
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2018-12-24 23:56:09 +0530

    Added reference import from csv, but, buggy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
 if AdminUser.find_by(email: 'admin@example.com').nil?
 	AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 else
 	puts "User already exists"
 end
-'''
+
 Design.delete_all
 Overview.delete_all
 Business.delete_all
 Installation.delete_all
 Manufacturing.delete_all
 Performence.delete_all
+Reference.delete_all
+
 
 Product.delete_all
 Family.delete_all
-'''
+
 html_str = '<p><big> '
 html_str2 = ' HTML<br /> &nbsp; <p>&nbsp;</p> <p><strong><img alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf5Ds6M2i--Q0IKiKxt8P-cyYL4CAqkOkJT7iU9RlHWt29eIb5" style="width: 275px; height: 183px;" /></strong></p> </p"<big>'
 
@@ -212,25 +216,19 @@ else
 end	
 
 
-if Reference.count == 0
-	Reference.create!(image: "/reference/1.jpg",customer: "MR.SAMPATH SINGH",project_type: "Residential",
-		project: "RESIDENTIAL HOME",product: "GLOSS MEISTER",code: "21027 IRISH CREAM",
-		 location: "HOUSE NO.1005 SECTOR 27 CHD", quantity: "25 SHEETS", year: 2018)
-	Reference.create!(image: "/reference/2.jpg",customer: "MR. HIRDIYA NAND",project_type: "Residential",
-		project: "RESIDENTIAL HOME",product: "GLOSS MEISTER,LAM",code: "25391,21055,21055 HGL",
-		 location: "Residential	HOUSE NO.3110 SECTOR 27 CHD", quantity: "4,2,1 SHEET", year: 2018)
-	Reference.create!(image: "/reference/3.jpg",customer: "GEE ARR INTERIOR",project_type: "Residential",
-		project: "RESIDENTIAL HOME",product: "GLOSS MEISTER",code: "21065, 25139",
-		 location: "Residential	RESIDENTIAL HOME", quantity: "4", year: 2017)
+CSV.foreach(Rails.root.join('db', 'data', 'references.csv'), headers: true) do |row|
+  Reference.create! do |t|
+	t = Reference.new
+	t.image = "/reference/1.jpg"
+	t.customer = row["customer"]
+	t.project_type = row["project_type"]
+	t.project = row["project"]
+	t.product = row["product"]
+	t.code = row["code"]
+	t.location = row["location"]
+	t.quantity = row["quantity"]
+	t.year = row["year"]
+	t.save
+  end
+end
 
-	Reference.create!(image: "/reference/4.png",customer: "MR.SAMPATH SINGH",project_type: "Commercial",
-		project: "COMMERCIAL D2",product: "GLOSS MEISTER",code: "21027 IRISH CREAM",
-		 location: "HOUSE NO.1005 SECTOR 27 CHD", quantity: "25 SHEETS", year: 2018)
-	Reference.create!(image: "/reference/5.jpg",customer: "MR. HIRDIYA NAND",project_type: "Commercial",
-		project: "COMMERCIAL D1",product: "GLOSS MEISTER,LAM",code: "25391,21055,21055 HGL",
-		 location: "Residential	HOUSE NO.3110 SECTOR 27 CHD", quantity: "4,2,1 SHEET", year: 2018)
-	Reference.create!(image: "/reference/6.jpg",customer: "GEE ARR INTERIOR",project_type: "Commercial",
-		project: "COMMERCIAL D1",product: "GLOSS MEISTER",code: "21065, 25139",
-		 location: "Residential	RESIDENTIAL HOME", quantity: "4", year: 2017)
-	puts "Reference Created"
-end	
