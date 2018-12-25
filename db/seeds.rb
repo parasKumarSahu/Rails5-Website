e3a657d637eb5375bdaeb31a9a5f3205ab0b3fc0
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2018-12-25 12:53:21 +0530

    fixed fast fade ou

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ if AdminUser.find_by(email: 'admin@example.com').nil?
 else
 	puts "User already exists"
 end
-'''
+
 
 Design.delete_all
 Overview.delete_all
@@ -23,215 +23,63 @@ Performence.delete_all
 
 Product.delete_all
 Family.delete_all
-'''
+
 html_str = '<p><big> '
 html_str2 = ' HTML<br /> &nbsp; <p>&nbsp;</p> <p><strong><img alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf5Ds6M2i--Q0IKiKxt8P-cyYL4CAqkOkJT7iU9RlHWt29eIb5" style="width: 275px; height: 183px;" /></strong></p> </p"<big>'
 
-if Family.find_by(name: 'HPL').nil?
-	puts "Creating HPL Products"
-	f = Family.create!(name: 'HPL')
-
-	pro = f.products.create! name: 'LAM'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-	pro = f.products.create! name: 'MR'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-
-	pro = f.products.create! name: 'MRmax'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-else
-	puts "HPL already exists"
+if Family.count == 0
+	csv_text = File.read(Rails.root.join('db', 'data', 'product.csv'))
+	csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+	csv.each do |row|
+		if row["family"].length >=2
+			t = Family.find_by name: row["family"]
+			if t == nil
+				t = Family.new
+				t.name = row["family"]
+				t.save
+			end
+			product_name = row["product"]
+			product_name = product_name.gsub("+", "plus")
+			t.products.create! name: product_name
+			pro = Product.find_by name: product_name
+			pro.create_design(selector: html_str+"selector"+html_str2,
+			 guide: html_str+"guide"+html_str2,
+			  area: html_str+"area"+html_str2,
+			   sectors: html_str+"sectors"+html_str2,
+			    finishes: html_str+"finishes"+html_str2,
+			     availability: html_str+"availability"+html_str2)
+			pro.create_installation(safety: html_str+"safety"+html_str2,
+			 fabrication: html_str+"fabrication"+html_str2)
+			pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
+			 process: html_str+"process"+html_str2)
+			pro.create_performence(features: html_str+"features"+html_str2,
+			 maintenance: html_str+"maintenance"+html_str2,
+			  warranty: html_str+"warranty"+html_str2)
+			pro.create_overview(body: html_str+"Overview"+html_str2) 
+			pro.create_business(body: html_str+"Business"+html_str2)
+			puts "created #{row["family"]} > #{row["product"]}"
+		end
+	end
 end	
 
 
+#Reference.delete_all
 
-
-
-if Family.find_by(name: 'PPL').nil?
-	puts "Creating PPL Products"
-	f = Family.create!(name: 'PPL')
-
-	pro = f.products.create! name: 'LAMo'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-	pro = f.products.create! name: 'MRo'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-
-	pro = f.products.create! name: 'MRmaxo'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-else
-	puts "PPL already exists"
-end	
-
-
-
-
-if Family.find_by(name: 'KPL').nil?
-	puts "Creating KPL Products"
-	f = Family.create!(name: 'KPL')
-
-	pro = f.products.create! name: 'LAMi'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-	pro = f.products.create! name: 'MRi'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-
-	pro = f.products.create! name: 'MRmaxi'
-	pro.create_design(selector: html_str+"selector"+html_str2,
-	 guide: html_str+"guide"+html_str2,
-	  area: html_str+"area"+html_str2,
-	   sectors: html_str+"sectors"+html_str2,
-	    finishes: html_str+"finishes"+html_str2,
-	     availability: html_str+"availability"+html_str2)
-	pro.create_installation(safety: html_str+"safety"+html_str2,
-	 fabrication: html_str+"fabrication"+html_str2)
-	pro.create_manufacturing(inputs: html_str+"inputs"+html_str2,
-	 process: html_str+"process"+html_str2)
-	pro.create_performence(features: html_str+"features"+html_str2,
-	 maintenance: html_str+"maintenance"+html_str2,
-	  warranty: html_str+"warranty"+html_str2)
-	pro.create_overview(body: html_str+"Overview"+html_str2) 
-	pro.create_business(body: html_str+"Business"+html_str2)
-
-else
-	puts "KPL already exists"
-end	
-
-Reference.delete_all
-
-
-count = 0
-csv_text = File.read(Rails.root.join('db', 'data', 'references.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
-csv.each do |row|
-	t = Reference.new
-	t.image = "/reference/1.jpg"
-	t.project_type = row["type"]
-	t.project = row["project"]
-	t.product = row["product"]
-	t.code = row["code"]
-	t.location = row["location"]
-	t.quantity = row["quantity"]
-	t.customer = row["customer"]
-	t.year = row["year"].to_i
-	t.save
-	puts row["customer"]
+if Reference.count == 0
+	csv_text = File.read(Rails.root.join('db', 'data', 'references.csv'))
+	csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+	csv.each do |row|
+		t = Reference.new
+		t.image = "/reference/1.jpg"
+		t.project_type = row["type"]
+		t.project = row["project"]
+		t.product = row["product"]
+		t.code = row["code"]
+		t.location = row["location"]
+		t.quantity = row["quantity"]
+		t.customer = row["customer"]
+		t.year = row["year"].to_i
+		t.save
+		puts row["customer"]
+	end
 end
-
