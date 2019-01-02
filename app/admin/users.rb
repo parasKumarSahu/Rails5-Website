66821ec8bc6cacbf091c032b5f65613f1b21d3e3
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2019-01-02 16:52:34 +0530

    Added Password Config

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,7 +2,8 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at,
+	:failed_attempts, :unlock_token, :locked_at
 
 #
 # or
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_one :user_info       
+	devise :database_authenticatable, :registerable,
+	     :recoverable, :rememberable, :validatable, :lockable
+	has_one :user_info       
+
+
+	validate :password_complexity
+
+	def password_complexity
+		return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,70}$/
+
+		errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase and 1 digit'
+	end
+
 end
+
